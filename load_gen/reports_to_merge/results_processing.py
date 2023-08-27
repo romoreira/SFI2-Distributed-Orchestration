3e8524d5d56c79965a860190c1cd20e753b1ba06
romoreira <rodrigo.moreira206@gmail.com>
2023-08-27 15:37:08 -0300

    working on: read patial results.

--- load_gen/reports_to_merge/results_processing.py
+++ load_gen/reports_to_merge/results_processing.py
@@ -186,7 +186,10 @@ data.index = data['time']
 data.set_index('time', inplace=True)
 
 data = data.head(1000).copy()
-#data = data.interpolate(method='linear')
+
+window_size = 2  # Defina o tamanho da janela móvel
+data = data.rolling(window=window_size).mean()
+
 
 
 #data =  reduce_dataframe(data)
@@ -220,8 +223,6 @@ plt.grid(False)
 #plt.show()
 plt.savefig(directory+str(model_name)+'_training_test_split.pdf', bbox_inches = 'tight', pad_inches = 0.1)
 #print(data)
-
-
 
 
 data.rename(columns={'mean': 'target'}, inplace=True)
