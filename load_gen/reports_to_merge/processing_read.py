02c041fe5b4b5ab2937b9460e4f741cab5124fc2
romoreira <rodrigo.moreira206@gmail.com>
2023-08-28 18:02:03 -0300

    working on: read partial results.

--- load_gen/reports_to_merge/processing_read.py
+++ load_gen/reports_to_merge/processing_read.py
@@ -188,7 +188,7 @@ data.set_index('time', inplace=True)
 print("Dataset SIZE> "+str(data.shape))
 
 #divide data into train and test
-train_ind = int(len(data)*0.8)
+train_ind = int(len(data)*0.9)
 train = data[:train_ind]
 test = data[train_ind:]
 #print(train.head())
